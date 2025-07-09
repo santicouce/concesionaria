1e59509761a6b82ee3a36ebd51790ccb24c321b0
Santiago <santicouce@outlook.com>
2025-07-09 01:55:53 -0300

    elimino archivos que se movieron a nuevos servicios

--- src/main/java/ar/edu/palermo/concesionaria/repositorio/EmpleadoRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.palermo.concesionaria.repositorio;
-
-import ar.edu.palermo.concesionaria.dominio.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
-}
--- src/main/java/ar/edu/palermo/concesionaria/repositorio/VehiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.palermo.concesionaria.repositorio;
-
-import ar.edu.palermo.concesionaria.dominio.Vehiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehiculoRepository extends JpaRepository<Vehiculo, Integer> {
-}
